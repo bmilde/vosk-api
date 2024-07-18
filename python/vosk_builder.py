df02e8c322ef3d3723d7a15356b3f5b216802577
bmilde <bmilde@users.noreply.github.com>
2024-07-19 00:34:15 +0200

    Update vosk_builder.py

--- python/vosk_builder.py
+++ python/vosk_builder.py
@@ -3,11 +3,16 @@
 import os
 from cffi import FFI
 
-vosk_root=os.environ.get("VOSK_SOURCE", "..")
+vosk_root = os.environ.get("VOSK_SOURCE", "..")
 cpp_command = "cpp " + vosk_root + "/src/vosk_api.h"
 
 ffibuilder = FFI()
-ffibuilder.set_source("vosk.vosk_cffi", None)
+ffibuilder.set_source(
+    "vosk.vosk_cffi",
+    None,
+    libraries=['gfortran'],  # Add gfortran to the list of libraries
+    extra_link_args=['-lgfortran']  # Add the linker flag
+)
 ffibuilder.cdef(os.popen(cpp_command).read())
 
 if __name__ == '__main__':
